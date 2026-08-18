60c4f5b8c6b148ba4429a2a311ce79f527b81ca7
Rol <dnalor525@gmail.com>
2026-08-18 11:10:44 +0300

    Bump app version to 0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.tractorfieldguide"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.1"
+        versionCode = 3
+        versionName = "0.2"
     }
     signingConfigs {
         create("tractorRelease") {
